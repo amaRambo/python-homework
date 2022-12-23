4b0e2b238fdcaa451df7b3b6f637466216f4adbc
ilya <ilya_1_3_5@mail.ru>
2022-12-23 16:03:00 +0500

    4 5 added

--- sem3/task4.py
+++ sem3/task4.py
@@ -3,29 +3,13 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
-import math
-a = int(input('введите число: '))
-count = a
-while count > 1:
-    b = 
-    count = math.floor(count/2)
-
-  #
-    # *Пример:*
-    #
-    # - 45 -> 101101
-    # - 3 -> 11
-    # - 2 -> 10
-# def dec_to_bin(num, result = ""):
-#     if num == 0:
-#         return result[::-1]
-#     result += str(num%2)
-#     return dec_to_bin(num//2,result)
-#
-#
-# n = int(input())
-#
-# if n!=0:
-#     print(dec_to_bin(n))
-# else:
-#     print(0)
+def dvoich(num, result = ""):
+  if num == 0:
+    return result[::-1]
+  result += str(num%2)
+  return dvoich(num//2,result)
+n = int(input())
+if n!=0:
+  print(dvoich(n))
+else:
+  print(0)
--- sem3/task5.py
+++ sem3/task5.py
@@ -1,14 +1,14 @@
-# def postive_fib(n):
-#     postive_list = [0,1]
-#     for i in range(n-1):
-#         postive_list.append(postive_list[-2]+postive_list[-1])
-#     return postive_list
-# 
-# def negative_fiv(n):
-#     negative_list = [0,1]
-#     for i in range(n-1):
-#         negative_list.append(negative_list[-2]-negative_list[-1])
-#     return negative_list
-# 
-# 
-# print(negative_fiv(8)[::-1] + postive_fib(8)[1:])
+def postive_fib(n):
+    postive_list = [0,1]
+    for i in range(n-1):
+        postive_list.append(postive_list[-2]+postive_list[-1])
+    return postive_list
+
+def negative_fiv(n):
+    negative_list = [0,1]
+    for i in range(n-1):
+        negative_list.append(negative_list[-2]-negative_list[-1])
+    return negative_list
+
+
+print(negative_fiv(8)[::-1] + postive_fib(8)[1:])
