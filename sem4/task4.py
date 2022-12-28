4baf8ec10d6b2d38bace440e3bdfac254212c888
Ilya Tsai <ilya_1_3_5@mail.ru>
2022-12-28 19:01:59 +0500

    123

--- sem4/task4.py
+++ sem4/task4.py
@@ -4,5 +4,9 @@
 # многочлена и записать в файл многочлен степени k.
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
-
+import random
 k = int(input('k= '))
+f = open('sem4task4.txt', 'r')
+zapis = str(k)
+f.write(zapis)
+f.close
