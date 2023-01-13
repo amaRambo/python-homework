b86c3ca27419f0eb5d5b0be0e77cca5280178383
ilya <ilya_1_3_5@mail.ru>
2023-01-13 23:29:32 +0500

    5 hw added

---
+++ sem5/task4.py
@@ -0,0 +1,38 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+def compression(fComp):
+    count = 1
+    z = ""
+    fcomp = fComp.readline()
+    for i in range(len(fcomp)-1):
+        if fcomp[i] == fcomp[i+1]:
+            count = count + 1
+        else:
+            z = z + fcomp[i] + str(count)
+            count = 1
+    return z + fcomp[i] + str(count)
+
+
+def recovery(fRec):
+    f = fRec.readline()
+    z = ""
+    for i in range(0, len(f)-1, 2):
+        j = 0
+        while j < int(f[i+1]):
+            z = z + f[i]
+            j += 1
+    return z
+
+
+fComp = open("comp.txt", "r")
+fRec = open("rec.txt", "w")
+fRec.write(compression(fComp))
+fRec.close()
+fComp.close()
+fRec = open("rec.txt", "r")
+print("Возвращаем: ", recovery(fRec))
+fRec.close()
+
+
+
+
