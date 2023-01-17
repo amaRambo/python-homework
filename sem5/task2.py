6c723f8451364447ac909f1023ab27856e310d36
ilya <ilya_1_3_5@mail.ru>
2023-01-17 09:25:09 +0500

    дополнени

--- sem5/task2.py
+++ sem5/task2.py
@@ -38,3 +38,83 @@ while i <= n:
 
 
 
+# import random
+# while not 0<turn<max_sweet:
+
+# def pve_s():
+#     name1 = input("Введите имя первого игрока - ")
+#     name2 = "bot"
+#     sweets = int(input("Введите желаемое колчиество конфет - "))
+#     max_sweet = int(input("Введите максимум конфет за ход"))
+#
+#     first_turn = random.choice([name1, name2])
+#     flag = name1 if first_turn == name1 else name2
+#
+#     while sweets>0:
+#         print(f"Ваш ход {flag}")
+#
+#         if flag == name1:
+#             turn = int(input("введите желаемое количество конфет для взятия- "))
+#
+#             while not 0<turn<max_sweet:
+#                 print("Введите конфеты в диапазоне от 0 до", max_sweet)
+#                 turn = int(input("введите желаемое количество конфет для взятия- "))
+#
+#             sweets -= turn
+#             if sweets>0:
+#                 print(f'конфет осталось - {sweets}')
+#             else:
+#                 print(f'конфет осталось - 0')
+#
+#             flag = name2 if flag == name1 else name1
+#         else:
+#             turn = random.randint(1,max_sweet)
+#             print(f"bot взял {turn} конфет")
+#             sweets -= turn
+#             if sweets > 0:
+#                 print(f'конфет осталось - {sweets}')
+#             else:
+#                 print(f'конфет осталось - 0')
+#
+#             flag = name2 if flag == name1 else name1
+#
+#     winner = name2 if flag == name1 else name1
+#     print(f"Поздравляем победил игрок {winner}")
+#
+#
+
+# def pve_smart():
+#     name1 = input("Введите имя первого игрока - ")
+#     name2 = "bot"
+#     sweets = int(input("Введите желаемое колчиество конфет - "))
+#     max_sweet = int(input("Введите максимум конфет за ход"))
+#
+#     first_turn = random.choice([name1, name2])
+#     flag = name1 if first_turn == name1 else name2
+#
+#     while sweets>0:
+#         print(f"Ваш ход {flag}")
+#
+#         if flag == name1:
+#             turn = int(input("введите желаемое количество конфет для взятия- "))
+#
+#         else:
+#             if sweets == max_sweet:
+#                 turn = sweets
+#             elif sweets%max_sweet==0:
+#                 turn = max_sweet-1
+#             else:
+#                 turn = sweets%max_sweet-1
+#             print(f"bot взял {turn} конфет")
+#             sweets -= turn
+#             if sweets > 0:
+#                 print(f'конфет осталось - {sweets}')
+#             else:
+#                 print(f'конфет осталось - 0')
+#
+#             flag = name2 if flag == name1 else name1
+#
+#     winner = name2 if flag == name1 else name1
+#     print(f"Поздравляем победил игрок {winner}")
+#
+# pve_smart()
--- sem5/task3.py
+++ sem5/task3.py
@@ -28,4 +28,73 @@ for count in range(1,10):
         matr[strok,stolb] = "X"
       
 
+# import random
+#
+# maps = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# victories = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+#
+#
+# def check_victories():
+#     for i in victories:
+#         if maps[i[0]] == maps[i[1]] == maps[i[2]]:
+#             return True
+#         else:
+#             return False
+#
+# def print_maps():
+#     print(maps[0], end=" ")
+#     print(maps[1], end=" ")
+#     print(maps[2])
+#
+#     print(maps[3], end=" ")
+#     print(maps[4], end=" ")
+#     print(maps[5])
+#
+#     print(maps[6], end=" ")
+#     print(maps[7], end=" ")
+#     print(maps[8])
+#
+# print_maps()
+# import random
+#
+# maps = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+# victories = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+#
+# name1 = input("Введите имя первого игрока - ")
+# name2 = input("Введите имя второго игрока - ")
+#
+#
+# first_turn = random.choice([name1,name2])
+#
+# print(first_turn)
+# game_over = False
+#
+# cur_turn = first_turn
+#
+# while not game_over:
+#     if cur_turn == first_turn:
+#         symbol = "X"
+#         step = int(input("введи клетку от 1 до 9, куда хочешь походить"))
+#         if maps[int(step) - 1] == "X" or maps[int(step) - 1] == "0":
+#             print("Occupied!!! Try Again")
+#             continue
+#         maps[step-1] = symbol
+
+#     else:
+#         symbol = "0"
+#         step = int(input("введи клетку от 1 до 9, куда хочешь походить"))
+#         if maps[int(step) - 1] == "X" or maps[int(step) - 1] == "0":
+#             print("Occupied!!! Try Again")
+#             continue
+#         maps[step - 1] = symbol
+#     print_maps()
+#     if check_victories():
+#         print(f"победил игрок {cur_turn}")
+#         game_over = True
+#     cur_turn = name2 if cur_turn == name1 else name1
+
+
+
 
