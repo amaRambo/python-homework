d8b74272350a70297a42b43541044d48ae20a30d
Ilya Tsai <ilya_1_3_5@mail.ru>
2023-01-24 22:45:09 +0500

    7 fix

--- sem7/import_file.py
+++ sem7/import_file.py
@@ -2,7 +2,7 @@ from view_file import for_import
 
 def import_fun(a):
     file = open('sem7/user.csv', 'a')
-    file.writelines(a) 
+    file.write(a + '\n') 
     file.close()
     
 # import_fun(for_import())
