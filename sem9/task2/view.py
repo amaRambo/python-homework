8d773d867a24b9459d268b39caa5f07d4f28a6e0
Ilya Tsai <ilya_1_3_5@mail.ru>
2023-01-31 15:12:46 +0500

    act

--- sem9/task2/view.py
+++
@@ -1,7 +0,0 @@
-import telebot
-from telebot import types
-
-bot = telebot.TeleBot("5804587400:AAELkmE4RmkQKUl6745HozMTO-DfSZfH0d4")
-
-def enter(a):
-    bot.send_message(message.chat.id,"введи два числа через пробел")
